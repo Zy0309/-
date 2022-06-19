9ae392897d256cab1a9626a0480c49979091235e
“周杨” <133133605@qq.com>
2022-06-19 20:39:40 +0800

    123

---
+++ 日考联系/app/src/layout/layout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="layout">
+    <main>
+      <router-view></router-view>
+    </main>
+    <footer></footer>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
